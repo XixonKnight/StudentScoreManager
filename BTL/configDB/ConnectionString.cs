57eb238ea8a60a4a8939f01deded468c00b79672
Nhanbn98 <nhanbn98@gmail.com>
2021-03-02 23:50:56 +0700

    config-database

---
+++ BTL/configDB/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL.configDB
+{
+    class ConnectionString
+    {
+        public static string connectionString = "Data Source=DESKTOP-RMQQQBT;Initial Catalog=QuanLySinhVien;Persist Security Info=True;User ID=sa;Password=0363340096";
+    }
+}
