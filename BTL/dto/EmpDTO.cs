e3af54545f4d405e14f489a6b841e01ae1989ef9
Nhanbn98 <nhanbn98@gmail.com>
2021-03-16 21:23:20 +0700

    create-crystal-report

---
+++ BTL/dto/EmpDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL.dto
+{
+    class EmpDTO
+    {
+        public int id { get; set; }
+        public string fullName { get; set; }
+        public string position { get; set; }
+        public string Department { get; set; }
+        public string address { get; set; }
+        public string email { get; set; }
+        public string academicLevel { get; set; }
+        public string phone { get; set; }
+        public string salary { get; set; }
+    }
+}
